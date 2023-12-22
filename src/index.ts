df3e41e279289de837502e098498da29393e3a02
Micah Henning <github@micah.soy>
2023-12-22 23:18:41 +0000

    feat: add prng bytes and uuid functionality. change icons to mit-licensed phosphor

--- src/index.ts
+++ src/index.ts
@@ -16,15 +16,16 @@ menuItems.forEach((item) => item.addEventListener('click', (event) => {
   sections.forEach(i => i.classList.remove('active'));
   item.classList.add('active');
   if (item.dataset.target) document.querySelector(item.dataset.target)?.classList.add('active');
+  hideResult();
 }));
 
 /**
- * Digest Logic
+ * Result Logic
  */
 
 const showResult = (value: string) => {
   load(100);
-  const result = document.querySelector<HTMLElement>('#digest .result');
+  const result = document.querySelector<HTMLElement>('#result');
   if (result) {
     const content = result.querySelector('.content');
     if (content) content.textContent = value;
@@ -34,16 +35,16 @@ const showResult = (value: string) => {
 };
 
 const hideResult = () => {
-  const result = document.querySelector<HTMLElement>('#digest .result');
+  const result = document.querySelector<HTMLElement>('#result');
   if (result) result.style.opacity = '0';
 };
 
 const setCopyText = (value: string = 'Copy') => {
-  const copyText = document.querySelector('#digest .result .copy span');
+  const copyText = document.querySelector('#result .copy span');
   if (copyText) copyText.textContent = value;
 };
 
-const copyAnchor = document.querySelector('#digest .result > a.copy');
+const copyAnchor = document.querySelector('#result > a.copy');
 copyAnchor?.addEventListener('click', async (event) => {
   event.preventDefault();
   const parent = (event.currentTarget as HTMLElement).parentElement;
@@ -116,4 +117,86 @@ digestSelect?.addEventListener('change', (event) => {
 
 document.addEventListener('DOMContentLoaded', () => {
   digestSelect.dispatchEvent(new Event('change'));
+});
+
+/**
+ * Pseudo Random Number Generator Logic
+ */
+
+const prngOperation = document.querySelector('#random .operation') as HTMLSelectElement;
+const prngOptions = document.querySelector('#random .prng-options') as HTMLElement;
+const prngByteLength = document.querySelector('#random .byte-length') as HTMLInputElement;
+const prngOutput = document.querySelector('#random .output') as HTMLSelectElement;
+const prngOutputEncodingControl = document.querySelector('#random .output-encoding-control') as HTMLElement;
+const prngOutputEncoding = document.querySelector('#random .output-encoding') as HTMLSelectElement;
+const prngGenerateButton = document.querySelector('#random button') as HTMLButtonElement;
+
+prngOperation?.addEventListener('change', () => {
+  switch (prngOperation.selectedOptions[0].value) {
+    case 'random':
+      prngOptions.classList.add('active');
+      break;
+    case 'uuid':
+      prngOptions.classList.remove('active');
+      break;
+    default:
+  }
+});
+
+prngOutput?.addEventListener('change', () => {
+  switch (prngOutput.selectedOptions[0].value) {
+    case 'display':
+      prngOutputEncodingControl.classList.add('active');
+      break;
+    case 'download':
+      prngOutputEncodingControl.classList.remove('active');
+      break;
+    default:
+  }
+});
+
+const encode = (array: Uint8Array, radix: number): string => {
+  let padding: number = 0;
+  if (radix === 2) padding = 8;
+  if (radix === 8) padding = 3;
+  if (radix === 16) padding = 2;
+  if (!padding) throw new Error(`Radix ${radix} is not supported`);
+
+  return array.reduce<string>((str, byte) => str + byte.toString(radix).padStart(padding, '0'), '');
+};
+
+prngGenerateButton.addEventListener('click', () => {
+  switch (prngOperation.selectedOptions[0].value) {
+    case 'random':
+      const bytes = parseInt(prngByteLength.value, 10);
+      const array = new Uint8Array(bytes);
+      self.crypto.getRandomValues(array);
+
+      switch (prngOutput.selectedOptions[0].value) {
+        case 'download':
+          const blob = new Blob([array], { type: 'application/octet-stream' });
+          const uri = window.URL.createObjectURL(blob);
+
+          const a = document.createElement('a');
+          a.href = uri;
+          a.download = `prng-${bytes}-bytes.bin`;
+          document.body.appendChild(a);
+          a.click();
+
+          // Cleanup
+          document.body.removeChild(a);
+          window.URL.revokeObjectURL(uri);
+          break;
+        case 'display':
+          const radix = parseInt(prngOutputEncoding.selectedOptions[0].value, 10);
+          showResult(encode(array, radix));
+          break;
+        default:
+      }
+      break;
+    case 'uuid':
+      showResult(self.crypto.randomUUID());
+      break;
+    default:
+  }
 });
