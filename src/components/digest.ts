1fd81c32b394d859d22f70f0ab2b48df46a09d34
Micah Henning <github@micah.soy>
2024-01-01 20:28:02 +0000

    feat: enable digests of files. resolves #7

--- src/components/digest.ts
+++ src/components/digest.ts
@@ -18,17 +18,17 @@ import { ENCODING } from '../lib/encode';
 import load from '../lib/loader';
 import { hideResults, showResults } from '../lib/result';
 
+const digestSection = document.querySelector('#digest')!;
+
 /**
  * Character count
  */
-const input = document.querySelector('textarea');
-input?.addEventListener('input', () => {
-  const characterCount = document.querySelector('#digest .character-count');
-  if (characterCount) {
-    const count = input.value.length;
-    if (count === 1) characterCount.textContent = '1 character';
-    else characterCount.textContent = `${count} characters`;
-  }
+const textarea = digestSection.querySelector('textarea')!;
+textarea.addEventListener('input', () => {
+  const characterCount = digestSection.querySelector('.character-count')!;
+  const count = textarea.value.length;
+  if (count === 1) characterCount.textContent = '1 character';
+  else characterCount.textContent = `${count} characters`;
 });
 
 /**
@@ -42,10 +42,10 @@ async function digestMessage(message: string, algorithm: string) {
 
 let selected: DOMStringMap;
 
-const button = document.querySelector('button');
+const button = digestSection.querySelector('button');
 button?.addEventListener('click', async () => {
   load(0);
-  const text = input!.value;
+  const text = textarea.value;
   const algorithm = selected!.alg!;
   try {
     const digest = await digestMessage(text, algorithm);
@@ -53,9 +53,9 @@ button?.addEventListener('click', async () => {
   } catch (e) { handleError(e); }
 });
 
-const digestSelect = document.querySelector<HTMLSelectElement>('#digest-select')!;
+const digestSelect = digestSection.querySelector<HTMLSelectElement>('#digest-select')!;
 digestSelect.addEventListener('change', () => {
-  const menu = document.querySelector('#digest menu')!;
+  const menu = digestSection.querySelector('menu')!;
   const blockSize = menu.querySelector('#digest-block-size span')!;
   const method = menu.querySelector('#digest-method span')!;
   const specification = menu.querySelector('#digest-specification span')!;
@@ -68,6 +68,87 @@ digestSelect.addEventListener('change', () => {
   hideResults();
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  digestSelect.dispatchEvent(new Event('change'));
+/**
+ * File Uploads
+ */
+
+const digestFiles = (files?: FileList | null) => {
+  if (!files || !files.length) return;
+  load(0);
+
+  const list = Array.from(files);
+  const algorithm = digestSelect.selectedOptions[0].dataset.alg!;
+  const totalSize = list.reduce((size, file) => size + file.size, 0);
+  const currentSizes: number[] = [];
+  const digests: { label: string, value: ArrayBuffer }[] = [];
+
+  list.forEach((file, i) => {
+    const reader = new FileReader();
+
+    reader.onprogress = (event) => {
+      currentSizes[i] = event.loaded;
+      load((currentSizes.reduce((p, c) => p + c, 0) / totalSize) * 100);
+    };
+
+    reader.onload = async (event) => {
+      if (!event.target?.result || !(event.target.result instanceof ArrayBuffer)) {
+        handleError(new Error(`File [${file.name}] of type [${file.type}] failed to load.`));
+        return;
+      }
+
+      const label = `${algorithm} Digest of ${file.name} (${file.size.toLocaleString()} bytes; ${file.type || 'uknown type'})`;
+      digests.push({ label, value: await crypto.subtle.digest(algorithm, event.target.result) });
+
+      if (digests.length === files.length) showResults(digests);
+    };
+
+    reader.readAsArrayBuffer(file);
+  });
+};
+
+const upload = digestSection.querySelector<HTMLInputElement>('.upload input');
+upload?.addEventListener('change', () => digestFiles(upload.files));
+
+/**
+ * Drag-and-Drop File Uploads
+ */
+
+const preventDefault = (event: Event) => {
+  event.stopPropagation();
+  event.preventDefault();
+};
+
+const classState = (add: boolean) => () => {
+  if (add) textarea.classList.add('dragover');
+  else textarea.classList.remove('dragover');
+};
+
+textarea.addEventListener('dragenter', preventDefault);
+textarea.addEventListener('dragenter', classState(true));
+textarea.addEventListener('dragover', preventDefault);
+textarea.addEventListener('dragover', classState(true));
+textarea.addEventListener('dragleave', classState(false));
+textarea.addEventListener('dragend', classState(false));
+textarea.addEventListener('drop', (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+  classState(false)();
+  digestFiles(event.dataTransfer?.files);
 });
+
+/**
+ * Prevent drops outside of the textarea
+ */
+
+const disallowDrop = (event: DragEvent) => {
+  const e = event;
+  if (e.target !== textarea && e.dataTransfer) {
+    e.preventDefault();
+    e.dataTransfer.effectAllowed = 'none';
+    e.dataTransfer.dropEffect = 'none';
+  }
+};
+
+window.addEventListener('dragover', disallowDrop);
+window.addEventListener('dragenter', disallowDrop);
+window.addEventListener('drop', disallowDrop);
