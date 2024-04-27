de8459cc71c9820b741af847bec955467630704e
Patrick Organ <patrick@permafrost.dev>
2024-04-27 13:40:31 -0400

    add prettier config

---
+++ src/prettier.config.cjs
@@ -0,0 +1,33 @@
+const overrides = [
+  {
+    files: ['*.yml', '*.yaml'],
+    options: {
+      tabWidth: 2,
+    },
+  },
+  {
+    files: 'tsconfig.json',
+    options: {
+      parser: 'json',
+    },
+  },
+];
+
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  overrides,
+  printWidth: 160,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
